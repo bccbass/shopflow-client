3a5276a4d0e94b7fbbb0aa935d5ed1d7666fc0f9
bccbass <109984363+bccbass@users.noreply.github.com>
2024-12-07 08:37:42 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Loading from './Loading'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      < Loading />
+    </>
+  )
+}
+
+export default App
---
+++ src/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import './Loading.css'
+
+
+const Loading = () => {
+  return (
+    <div >
+        <h2 className='background text-3xl text-red-300 h-96 uppercase text-center w-screen'>Loading...</h2>
+    </div>
+  )
+}
+
+export default Loading
