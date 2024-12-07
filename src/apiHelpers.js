79b9c3605495ccc93d009519467b9cde94c68ad9
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2024-12-08 09:53:10 +1100

    working .env. added .env.sample file

--- src/apiHelpers.js
+++ src/apiHelpers.js
@@ -2,10 +2,11 @@
 // dotenv.config()
 
 
-// const url = import.meta.env.API_URL
-const url = "https://shopflow-api.onrender.com"
+const url = import.meta.env.VITE_SHOPFLOW_API
+// const url = "https://shopflow-api.onrender.com"
 export const getResource = async (route) => {
     try {
+      console.log(url);
       const resource = await fetch(`${url}/${route}`);
       if (!resource.ok){ throw new Error(`Response status: ${resource.status}`); }
       const json = await resource.json();
