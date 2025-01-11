495728ca890063c2f3dab83c70b8360ca894ae40
bccbass <109984363+bccbass@users.noreply.github.com>
2025-01-11 12:51:46 +1100

    added search to notes

--- src/Notes/Notes.jsx
+++ src/Notes/Notes.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react'
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router";
 import { getResource } from "../assets/apiHelpers";
@@ -6,15 +7,26 @@ import Note from "./Note";
 import Loading from "../Loading";
 import NoteForm from "./NoteForm";
 import SectionHeader from "../SectionHeader";
+import Search from '../Search'
 import { Box, Container } from "@mui/material";
 
 const Notes = () => {
 	const [searchParams, setSearchParams] = useSearchParams();
+  const [ searchTerm, setSearchTerm ] = useState('')
 	const sortOrder = searchParams.get("sort");
 	const notesQuery = useQuery({
 		queryKey: ["notes", sortOrder],
 		queryFn: () => getResource("notes?sort=" + sortOrder),
 	});
+
+const filterArray = (arr, searchTerm) => {
+    return searchTerm.length == 0 ?
+      arr :
+      arr.filter(note => [note.body, note.title, note.createdBy]
+      .map(val => val.toLowerCase().includes(searchTerm.toLowerCase()))
+        .includes(true)
+    )
+  }
 
 	return (
 		< Container sx={{m:0}}>
@@ -22,6 +34,18 @@ const Notes = () => {
 				setSearchParams={ setSearchParams }
 				title="Notes"
 			/>
+
+             <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-end",
+                mb: 4,
+                pr: 2
+              }}
+            >
+              < Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </Box>
 			<Box sx={{display: 'flex', flexWrap: 'wrap'}}>
 				<NoteForm key={'noteForm'}/>
 				{notesQuery.isLoading ? (
@@ -29,7 +53,7 @@ const Notes = () => {
 				) : notesQuery.isError ? (
 					<h1 className="">Error</h1>
 				) : (
-					notesQuery?.data?.map((note) => (
+					filterArray(notesQuery.data, searchTerm).map((note) => (
 						<Box key={note._id}>
 							<Note note={note} />
 						</Box>
