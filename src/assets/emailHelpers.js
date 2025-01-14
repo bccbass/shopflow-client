3834aff478d255c5b73ea22859cc0ac1da739e7d
bccbass <109984363+bccbass@users.noreply.github.com>
2025-01-14 19:38:52 +1100

    email format

--- src/assets/emailHelpers.js
+++ src/assets/emailHelpers.js
@@ -55,7 +55,7 @@ const generateTrialConfirmation = (student, info, admin) => {
   return {
     id: "trialConfirmation",
     label: "Trial Confirmation",
-    subject: `Trial Lesson Info for ${student.trialDay}, ${student.trialDate} at ${student.trialTime}`,
+    subject: `Trial Lesson Info for ${student.trialDay.split(' ')[0]}, ${student.trialDate} at ${student.trialTime}`,
     text: `Hi ${addressee},
      
 Welcome to our Caringbah Music family! We're so glad to have you. Please find the details of ${studentPossesive} ${instrument}lesson below:
