92267c18a507cf8139deb20227b45a3c032a1871
bccbass <109984363+bccbass@users.noreply.github.com>
2025-01-21 01:24:13 +1100

    fixed enrol followup order

--- src/Tables/EnrollmentFollowUpForm.jsx
+++ src/Tables/EnrollmentFollowUpForm.jsx
@@ -5,30 +5,59 @@ import { Box, Typography } from "@mui/material";
 const EnrollmentFollowUpForm = ({ lead }) => {
   return (
     <Box
-      sx={{ mt: 3, width: "100%", display: "flex", flexDirection: "column", alignItems: 'center' }}
+      sx={{
+        mt: 3,
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
     >
       <Box
         sx={{
           p: 3,
           width: "96%",
-          borderRadius: '12px',
+          borderRadius: "12px",
           display: "flex",
           flexDirection: "column",
           bgcolor: "teal",
         }}
       >
-        <Typography color="white" align="center" variant="h5" fontWeight={"bold"}>
+        <Typography
+          color="white"
+          align="center"
+          variant="h5"
+          fontWeight={"bold"}
+        >
           Enrollment Follow Up Checklist
         </Typography>
-        <Box sx={{ display: "flex", flexDirection: "row", alignItems: 'flex-start' }}>
-          {Object.keys(lead.enrollmentFollowUp).map((task, i) => (
-            <ToggleTaskStatusButton
-              key={i}
-              id={lead._id}
-              taskName={task}
-              taskStatus={lead.enrollmentFollowUp[task]}
-            />
-          ))}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+          }}
+        >
+          <ToggleTaskStatusButton
+            id={lead._id}
+            taskName="status"
+            taskStatus={lead.enrollmentFollowUp.status}
+          />
+          <ToggleTaskStatusButton
+            id={lead._id}
+            taskName="timetable"
+            taskStatus={lead.enrollmentFollowUp.timetable}
+          />
+          <ToggleTaskStatusButton
+            id={lead._id}
+            taskName="createInvoice"
+            taskStatus={lead.enrollmentFollowUp.createInvoice}
+          />
+          <ToggleTaskStatusButton
+            id={lead._id}
+            taskName="sentInvoice"
+            taskStatus={lead.enrollmentFollowUp.sentInvoice}
+          />
         </Box>
       </Box>
     </Box>
