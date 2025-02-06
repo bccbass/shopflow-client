087a48ac5e434a3a583729962d086225a490ec31
bccbass <109984363+bccbass@users.noreply.github.com>
2025-02-06 12:27:37 +1100

    lead anaylytics logic

--- src/NewStudents/NewStudents.jsx
+++ src/NewStudents/NewStudents.jsx
@@ -48,7 +48,7 @@ const NewStudents = () => {
         total: query.data.length,
         totalTrials: query.data.filter((lead) => lead.bookedTrial).length,
         completedTrials: query.data.filter(
-          (lead) => lead.trialLesson.date < now
+          (lead) => new Date(lead.trialLesson.date) < now
         ).length,
         totalEnrollments: query.data.filter((lead) => lead.enrolled).length,
       };
